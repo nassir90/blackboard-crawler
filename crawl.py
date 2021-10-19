b7d03f4e20b194036f752412ef6b61884aaf0d1c
Nozz <nozolo90@gmail.com>
2021-10-19 14:38:27 +0100

    Used my knowledge of JJeval to eliminate old numeric indexing of page elements

--- crawl.py
+++ crawl.py
@@ -50,49 +50,34 @@ async def try_login(page: Page):
 
     return False
 
+async def traverse_module(module_text: str, page: Page):
+    module = {"name" : module_text, "link" : module_link, "submodules" : []};
+
+    print("Traversing module %s" % module_text)
+    for submodule_link, submodule_text in await page.JJeval(SUBMODULE_LINK, "links => links.map(link => [link.href, link.innerText])"):
+        await page.goto(submodule_link)
+        module["submodules"].append(await traverse_submodule(submodule_link, submodule_text, page))
+
+    return module
+
 async def crawl(page: Page):
     modules = []
 
     await page.waitFor(1000)
     await page.goto("https://tcd.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1")
     
-    await page.waitForSelector(MODULE_LINK)
+    await page.waitForSelector("#agree_button", timeout=3000)
+    await page.click("#agree_button") # Need to accept privacy policy
+    agreed_to_cookies = True
 
-    for module_index in range(len(await page.JJ(MODULE_LINK))):
-        modules.append(await traverse_module(module_index, page))
-        await page.goto("https://tcd.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1")
+    for module_link, module_text in page.JJeval(MODULE_LINK, "links => links.map(link => [link.href, link.innerText])"):
+        await page.goto(module_link)
+        modules.append(await traverse_module(module_text, page))
 
     crawlfile = open(crawlfile_path, "w")
     json.dump(modules, crawlfile)
 
-async def traverse_module(index: int, page: Page):
-    global agreed_to_cookies
-    if not agreed_to_cookies:
-        await page.waitForSelector("#agree_button", timeout=3000)
-        await page.click("#agree_button") # Need to accept privacy policy
-        agreed_to_cookies = True
-
-    await page.waitForSelector(MODULE_LINK)
-    module_link, module_text = await page.JJeval(MODULE_LINK, "(links, index) => [links[index].href, links[index].innerText]", index)
-    module = {"name" : module_text, "link" : module_link, "submodules" : []};
-
-    if "COMP" in module_text:
-        print("Traversing module #%d : %s" % (index, module_text))
-    else:
-        return
-
-    await page.goto(module_link)
-
-    module_root = page.url
-    for submodule_index in range(len(await page.JJ(SUBMODULE_LINK))):
-        module["submodules"].append(await traverse_submodule(submodule_index, page))
-        await page.goto(module_root)
-
-    return module
-
-
-async def traverse_submodule(submodule_index: int, page: Page):
-    submodule_link, submodule_text = await page.JJeval(SUBMODULE_LINK, "(links, submodule_index) => [links[submodule_index].href, links[submodule_index].innerText]", submodule_index)
+async def traverse_submodule(submodule_text: str, page: Page):
     submodule = {
         "name" : submodule_text,
         "link" : submodule_link,
@@ -100,10 +85,8 @@ async def traverse_submodule(submodule_index: int, page: Page):
         "panoptoVideos" : [],
         "submodules" : [] # AAHHHHH
     }
-
-    await page.goto(submodule_link)
     
-    print(" Traversing submodule #" + str(submodule_index) + " :" + submodule_text)
+    print(" Traversing submodule '%s' " % submodule_text)
     
     indices = await index(page, "  ")
     if indices:
@@ -126,19 +109,16 @@ async def traverse_list(page: Page, level: str):
     indices = {"files" : [], "videos" : [], "submodules" : []}
 
     content_root = page.url
-    for content_index in range(len(await page.JJ(CONTENT))):
-        await page.waitForSelector(CONTENT, timeout=2000)
-        content = (await page.JJ(CONTENT))[content_index]
+    
+    for link, link_text, header in page.JJeval("%(0)s .details a, %(0)s h3 a" % {'0', CONTENT}, "links => links.map(a => [a.href, a.innerText, a.parentElement.tagName == 'H3'])"):
+        if "webapps" not in link:
+            indices["files"].append(await get_real_filename(link, await page.cookies(), level))
+        elif header and link not in page.url:
+            print(level + "Descending into : '%s'" % link_text)
+            await page.goto(link)
+            indices["submodules"].append(await index(page, level + " "))
+            await page.goto(content_root)
 
-        for link, link_text, header in await content.JJeval(".details a, h3 a", "links => links.map(a => [a.href, a.innerText, a.parentElement.tagName == 'H3'])"):
-            if "webapps" not in link:
-                indices["files"].append(await get_real_filename(link, await page.cookies(), level))
-            else:
-                if header and "webapps" in link and link not in page.url:
-                    print(level + "Descending into : '%s'" % link_text)
-                    await page.goto(link)
-                    indices["submodules"].append(await index(page, level + " "))
-                    await page.goto(content_root)
     return indices
 
 async def traverse_panopto_list(page: Page, level: str):
