74d1b7771250aa45721ff28c7d202ef18a66776a
Nozz <nozolo90@gmail.com>
2021-10-19 14:09:35 +0100

    Cleaned up root directory

---
+++ blackboard-crawler.py
@@ -0,0 +1,7 @@
+# Will run three steps
+# Check if crawl.json is here
+# Prompt.should_recrawl?
+#   Login here if the user wants to recrawl
+# Prompt.pruned_crawlfile
+# Download(pruned_crawlfile)
+#  Login here if the user didn't recrawl
