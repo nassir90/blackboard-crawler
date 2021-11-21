ea441b13e824674d3bcbe6cb31fb6cf7bed742d5
Nozz <nozolo90@gmail.com>
2021-11-21 15:18:31 +0000

    Using urllib3 now

--- download.py
+++ download.py
@@ -1,25 +1,25 @@
 import json
-import traceback
 import os
 import os.path
-import urllib.request
-import urllib.parse
 import re
 import ffmpeg
+import urllib3
+import urllib.parse.unquote as unquote
 
 current_output_dir = ""
 crawlfile_path = "crawl.json"
+http = urllib3.PoolManager()
         
 def download_panopto_stream(stream_url: str, link_text: str, level: str):
-    master_response = urllib.request.urlopen(stream_url)
-    master_data = master_response.read()
+    master_response = http.request("GET", stream_url)
+    master_data = master_response.data
     master = master_data.decode('utf-8')
 
     first_master_entry = re.findall(r"\d+/index\.m3u8", master, re.MULTILINE)[0]
 
     url = re.sub(r"master\.m3u8.*", "", stream_url) + first_master_entry
-    index_response = urllib.request.urlopen(url)
-    index_data = index_response.read()
+    index_response = http.request("GET", url)
+    index_data = index_response.data
     index = index_data.decode('utf-8')
 
     ts = current_output_dir + link_text.strip() + ".ts"
@@ -32,8 +32,8 @@ def download_panopto_stream(stream_url: str, link_text: str, level: str):
         if ts_file:
             print(level + 'Downloading part %d/%d' % (int(ts_file.strip(".ts")),  total_parts))
             part_url = re.sub(r"master\.m3u8.*", first_master_entry.split('/')[0] + '/' + ts_file, stream_url)
-            part_response = urllib.request.urlopen(part_url)
-            output_ts.write(part_response.read())
+            part_response = http.request("GET", part_url)
+            output_ts.write(part_response.data)
 
     output_ts.close()
 
@@ -47,23 +47,20 @@ def download_panopto_stream(stream_url: str, link_text: str, level: str):
         pass
 
 def download_file(url: str, s_session_id: str, level: str):
-    request = urllib.request.Request(url)
-    request.add_header("Cookie", "s_session_id=" + s_session_id)
     try:
-        response = urllib.request.urlopen(request, timeout=3)
+        response = http.request(url, headers={"Cookie", "s_session_id="+s_session_id, timeout=3)
     except Exception as e:
         print(level + "└" + str(e))
         return
-    output_file_path = os.path.basename(urllib.parse.unquote(response.url))
+    output_file_path = os.path.basename(unquote(url))
     if not output_file_path:
         output_file_path = os.path.basename(response.url.strip("/"))
     temp_file_path = output_file_path + ".uncompleted-write"
 
     if not os.path.isfile(output_file_path):
         print(level + "└" + output_file_path + " does not exist. Downloading...")
-        data = response.read()
         temp_file = open(temp_file_path, "wb")
-        temp_file.write(data)
+        temp_file.write(response.data)
         temp_file.close()
         os.rename(temp_file_path, output_file_path)
     else:
